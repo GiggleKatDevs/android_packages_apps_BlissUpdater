7bb12b3cb11246adbcd178eab41178ad8be3714a
GiggleKat <gigglekatrom@gmail.com>
2014-10-16 14:59:35 -0700

    changed packaged name

--- src/com/bliss/updater/utils/MD5.java
+++ src/com/bliss/updater/utils/MD5.java
@@ -7,7 +7,7 @@
  * or at https://www.gnu.org/licenses/gpl-2.0.txt
  */
 
-package com.bliss.updater.utils;
+package com.gigglekat.updater.utils;
 
 import android.text.TextUtils;
 import android.util.Log;
